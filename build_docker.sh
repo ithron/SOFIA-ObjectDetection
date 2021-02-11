4b7b0672daf35a65551384fae89938ece8ad2b37
Stefan Reinhold <sre@informatik.uni-kiel.de>
2021-02-11 17:41:08 +0100

    Update default docker image name

--- build_docker.sh
+++ build_docker.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-SOFIA_DOCKER_IMAGE="${SOFIA_DOCKER_IMAGE:?sofia-object-detection}"
+SOFIA_DOCKER_IMAGE="${SOFIA_DOCKER_IMAGE:=sreinhold/sofia-object-detection}"
 
 docker build -t ${SOFIA_DOCKER_IMAGE} --build-arg TARGET_UID=`id -u` --build-arg TARGET_GID=`id -g` ./
